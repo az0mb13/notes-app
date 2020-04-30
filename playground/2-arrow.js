043ddf1a5ba5dd38c7381a42b1a63c88dbe1c2f2
az0mb13 <qwertyuiop.dixit3@gmail.com>
2020-04-30 18:36:51 +0530

    Github updated

--- playground/2-arrow.js
+++
@@ -1,9 +0,0 @@
-const event = {
-    name: "Zombie apocalypse",
-    guestList: ['A', 'V', 'C'],
-    printGuestList() {
-        console.log('Guest list for ', + this.guestList);
-    }
-}
-
-event.printGuestList()
